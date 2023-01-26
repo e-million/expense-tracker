63a9d8532daec665b4bab0a9c7b4efb5f2d4df82
e-million <58177555+e-million@users.noreply.github.com>
2023-01-26 13:46:31 -0800

    completed
    Added a dynamic chart that is working with the year filter to show the expenses for the selected year

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
@@ -9,8 +10,8 @@ const ExpenseDate = (props) => {
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
-      <div className="expense-date__day">{day}</div>
       <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,42 @@
 import React, { useState } from "react";
+
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
-  const AmountChangeHandler = (event) => {
+  const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
 
-  const DateChangeHandler = (event) => {
+  const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   const submitHandler = (event) => {
@@ -24,7 +45,7 @@ const ExpenseForm = (props) => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      data: new Date(enteredDate),
+      date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
@@ -44,9 +65,6 @@ const ExpenseForm = (props) => {
             onChange={titleChangeHandler}
           />
         </div>
-      </div>
-
-      <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Amount</label>
           <input
@@ -54,12 +72,9 @@ const ExpenseForm = (props) => {
             min="0.01"
             step="0.01"
             value={enteredAmount}
-            onChange={AmountChangeHandler}
+            onChange={amountChangeHandler}
           />
         </div>
-      </div>
-
-      <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Date</label>
           <input
@@ -67,12 +82,14 @@ const ExpenseForm = (props) => {
             min="2019-01-01"
             max="2022-12-31"
             value={enteredDate}
-            onChange={DateChangeHandler}
+            onChange={dateChangeHandler}
           />
         </div>
       </div>
-
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
